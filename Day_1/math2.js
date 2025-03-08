c251516ca12598a778dcac66b89ec6076d489c5d
Mr--Prakash6381 <prakash875478@gmail.com>
2025-03-09 03:02:22 +0530

    Feat : The Math Second File

---
+++ Day_1/math2.js
@@ -0,0 +1,6 @@
+const math = require('./math');
+console.log("Add =" + math.add(5, 5));
+console.log("Sub =" + math.sub(5, 5));
+console.log("Mul =" + math.mul(5, 5));
+console.log("Div =" + math.div(5, 5));
+ 
