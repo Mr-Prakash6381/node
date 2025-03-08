f9d12a32b5dac0392a9e565591ea6f045b063036
Mr--Prakash6381 <prakash875478@gmail.com>
2025-03-09 03:00:58 +0530

    Feat : This file Basic of node

---
+++ Day_1/basic.js
@@ -0,0 +1,13 @@
+/*
+
+console.log("Welcome to node");
+console.log(global);
+const os=require('os');
+console.log(os);
+console.log(os.version);
+console.log(__dirrame);
+
+
+*/
+console.log("Welcome");
+
